93fc05c64bc6dcfebec9c73fdaa42221f002ca61
auooru <auooru@163.com>
2021-02-02 16:57:25 +0800

    update: auth facade

--- src/Facade/Auth.php
+++ src/Facade/Auth.php
@@ -12,11 +12,8 @@ use Zxin\Think\Auth\Contracts\Authenticatable;
  * @package Zxin\Think\Auth\Facade
  * @method AuthGuard instance() static
  * @method int|string id() static
- * @method int userGenre() static
- * @method int userRoleId() static
  * @method Authenticatable user() static
  * @method bool check() static
- * @method bool can(string $name) static
  */
 class Auth extends Facade
 {
