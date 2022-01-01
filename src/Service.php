d63091c632378efdadf6dbb569c59e673d81ecb9
auooru <auooru@outlook.com>
2022-01-01 13:08:46 +0800

    update: 改进守卫提供商实现

--- src/Service.php
+++ src/Service.php
@@ -9,8 +9,13 @@ use Zxin\Think\Auth\Contracts\Authenticatable;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Doctrine\Common\Annotations\Reader;
 use think\App;
+use Zxin\Think\Auth\Contracts\Guard;
 use function array_keys;
 use function class_exists;
+use function get_class;
+use function is_object;
+use function is_string;
+use function is_subclass_of;
 
 class Service extends \think\Service
 {
@@ -27,10 +32,7 @@ class Service extends \think\Service
         if ($middleware && class_exists($middleware)) {
             $this->app->middleware->add($middleware, 'route');
         }
-        $guard = $this->app->config->get('auth.guardProvider');
-        // todo 临时方案
-        $this->app->bind(AuthGuard::class, $guard ?? AuthGuard::class);
-        $this->app->bind('auth', $guard ?? AuthGuard::class);
+        $this->registerGuard();
         $this->app->bind('auth.permission', Permission::class);
         $this->registerAccessGate();
 
@@ -41,6 +43,40 @@ class Service extends \think\Service
     public function boot()
     {
         Permission::getInstance();
+    }
+
+    protected function registerGuard()
+    {
+        $this->app->bind(Guard::class, function (App $app) {
+            return $this->guardInstance($app);
+        });
+        $this->app->bind(AuthGuard::class, Guard::class);
+        $this->app->bind('auth', Guard::class);
+    }
+
+    protected function guardInstance(App $app): Guard
+    {
+        $guard = $app->config->get('auth.guardProvider');
+        if (empty($guard)) {
+            return $app->invokeClass(AuthGuard::class);
+        } elseif (is_string($guard)) {
+            if (is_subclass_of($guard, Guard::class)) {
+                return $app->invokeClass($guard);
+            } else {
+                throw new \RuntimeException("invalid guard: {$guard}");
+            }
+        } elseif ($guard instanceof \Closure) {
+            $instance = $app->invokeFunction($guard);
+            if (!is_object($instance)) {
+                throw new \RuntimeException("invalid guard, not an object");
+            }
+            if (!($instance instanceof Guard)) {
+                throw new \RuntimeException('invalid guard: ' . get_class($instance));
+            }
+            return $instance;
+        } else {
+            throw new \RuntimeException("invalid guard provider");
+        }
     }
 
     protected function registerAccessGate()
